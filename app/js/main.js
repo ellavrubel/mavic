2d8fe9116621a4c1b975c4f6b3b4635384999d41
ellavrubel <webella@protonmail.com>
2020-08-29 18:40:42 +0300

    p2 finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function(){
 
+    $('.products__slider').slick({
+
+        infinite: false,
+
+        prevArrow: '<button class="slick-prev slick-arrow products__slider-btn slider-btnLeft">' +
+            '<svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path d="M9.21839 1L1 9L9.21839 17"/>\n' +
+            '</svg>\n</button>',
+        nextArrow: '<button class="slick-prev slick-arrow products__slider-btn slider-btnRight">' +
+            '<svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path d="M0.78161 17L9 9L0.78161 1"/>\n' +
+            '</svg>' +
+            '</button>'
+
+    });
+
 
 
   
