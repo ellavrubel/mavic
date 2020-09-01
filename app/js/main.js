d25896287dbbba65d7e1045a8f20abec8930db94
ellavrubel <webella@protonmail.com>
2020-09-01 17:00:02 +0300

    questions finished

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,23 @@ $(function(){
 
     });
 
+    $('.questions__item-title').on('click',function (){
+
+        $('.questions__item').removeClass('questions__item--active');
+
+
+        $(this).parent().addClass('questions__item--active');
+
+});
+
+
+
+
+
+
+
+
+
 
 
   
